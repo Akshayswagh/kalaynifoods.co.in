10d9791c1e6e489e5e5dc640e57d1906fb30c4f1
Akshayswagh <waghakshay186@gmail.com>
2025-04-09 09:30:14 +0530

    some changes done

--- public/js/contactUs.js
+++ public/js/contactUs.js
@@ -1,18 +1,19 @@
 function initMap() {
-    // The location of Kalyani Ice Cream Parlour
-    const kalyaniIceCreamParlour = { lat: 19.8399, lng: 75.8868 };
-    // The map, centered at Kalyani Ice Cream Parlour
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 15,
-      center: kalyaniIceCreamParlour,
-    });
-    // The marker, positioned at Kalyani Ice Cream Parlour
-    const marker = new google.maps.Marker({
-      position: kalyaniIceCreamParlour,
-      map: map,
-    });
-  }
-  
-  // Load the map when the page loads
-  window.onload = initMap;
-  
+  // The location of Kalyani Ice Cream Parlour
+  const kalyaniIceCreamParlour = { lat: 19.8399, lng: 75.8868 };
+  // The map, centered at Kalyani Ice Cream Parlour
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 15,
+    center: kalyaniIceCreamParlour,
+  });
+  // The marker, positioned at Kalyani Ice Cream Parlour
+  const marker = new google.maps.Marker({
+    position: kalyaniIceCreamParlour,
+    map: map,
+  });
+}
+
+
+
+// Load the map when the page loads
+window.onload = initMap;
